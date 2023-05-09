6bd3215ade6a6b56e5304de33320d721749eb741
Jer1s <flqmbl@gmail.com>
2023-05-10 05:17:01 +0900

    Feat:

--- app.js
+++ app.js
@@ -24,6 +24,17 @@ app.use(
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
