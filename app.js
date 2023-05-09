341790c71ad91453e7ad0d82893deeebaab98a0e
Jer1s <flqmbl@gmail.com>
2023-05-10 04:54:19 +0900

    Feat: For dev

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const Post = db.Post;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://deploy-preview-2--taskcomm.netlify.app",
     methods: "GET, POST, OPTIONS, PUT, DELETE, PATCH",
     credentials: true,
   })
